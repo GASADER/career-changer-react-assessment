b4cbc73f6de1adbda522863764f5d7ca9f27849f
GASADER <kajorhkaen@gmail.com>
2023-04-19 11:08:21 +0700

    Add: Navber css and root css

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -4,11 +4,12 @@ import './Navber.css'
 const Navbar = () => {
     return (
         <>
+        <navbar>
             <ul className="navlink">
                 <li><a href={'/'}>Home</a></li>
                 <li><a href={'/owner'}>Owner</a></li>
             </ul>
-            <hr />
+        </navbar>
         </>
     )
 }
